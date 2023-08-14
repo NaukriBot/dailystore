34ff4c5621b98d09379d467abe6a16e2dc9c314e
Gnanapragasam V <pragasam001@gmail.com>
2023-08-14 22:21:18 +0530

    Code refactor for header adn side bar layout

--- src/app/shared/toolbar/toolbar.module.ts
+++ src/app/shared/toolbar/toolbar.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material/material.module';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './side-bar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
@@ -16,6 +18,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     HeaderComponent,
+    SidebarComponent
   ]
 })
 export class ToolbarModule { }
